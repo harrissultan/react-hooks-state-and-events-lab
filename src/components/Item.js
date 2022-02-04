4ff72e682e2e8a090dc06a33c81d3f703d5996c7
Harris Sultan <harris_sultan@outlook.com>
2022-02-04 12:46:03 -0500

    commit tree 8f36403f342dfd2732a2e74f87f4069b30495a67

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isOn, setIsOn] = useState(false)
+
+  function handeler(){
+    setIsOn((isOn)=>!isOn)
+  }
+  const cartClass = isOn ? "in-cart" : ""   ;
+  const cartLable = isOn ? "Remove from Cart" : "Add to Cart"; 
+  const btnClass  = isOn ? "remove" : "add";
+  
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={btnClass} onClick={handeler}>{cartLable}</button>
     </li>
   );
 }
